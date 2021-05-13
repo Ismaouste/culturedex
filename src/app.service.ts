a4ec08a057df5ca9d9dcb3eb120bb662e2343c5d
Ismael Rodmacq <ismael.rodmacq@gmail.com>
2021-05-13 17:45:17 +0200

    Fetch data from POP API

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    const response = await axios.get(
+      'https://data.culture.gouv.fr/api/records/1.0/search/?dataset=liste-des-immeubles-proteges-au-titre-des-monuments-historiques&q=&facet=reg&facet=dpt_lettre&geofilter.polygon=(48.63107626999893%2C6.03776416041101)%2C(48.60384047940872%2C6.32066210962976)%2C(48.75707704000831%2C6.168226806895385)',
+    );
+    console.log(response);
+    return response.data;
   }
 }
